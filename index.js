67f1033a634b15ef47e448f1c4f8132c838382ca
simsektalhasefik-art <simsektalhasefik@gmail.com>
2026-08-03 13:47:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-import { registerRootComponent } from 'expo';
 import React from 'react';
-import { AppRegistry, Platform } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 
-if (Platform.OS === 'web') {
-  AppRegistry.registerComponent('main', () => App);
-  const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication('main', { rootTag });
-} else {
-  registerRootComponent(App);
-}
+// Uygulamayı doğrudan web DOM'una kaydet
+AppRegistry.registerComponent('main', () => App);
+AppRegistry.runApplication('main', {
+  rootTag: document.getElementById('root') || document.getElementById('main'),
+});
