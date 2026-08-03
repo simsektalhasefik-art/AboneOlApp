3876c2dfabf1dc06acb74ba542c7b4b8fc6ac52b
simsektalhasefik-art <simsektalhasefik@gmail.com>
2026-08-03 14:24:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { registerRootComponent } from 'expo';
-import App from './App'; // (App dosyanız 'src/App.js' içindeyse './src/App' yapmalısınız)
+import App from './App';
 
 registerRootComponent(App);
