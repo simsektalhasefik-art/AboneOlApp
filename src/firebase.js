2d771883ebcc6ae551127744b5c250b71c634b16
simsektalhasefik-art <simsektalhasefik@gmail.com>
2026-08-07 12:21:03 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEwn9-z_WS1tZgwv1fCgBe-MBJeu85fU4",
-  authDomain: "cebin-pro.firebaseapp.com",
-  projectId: "cebin-pro",
-  storageBucket: "cebin-pro.firebasestorage.app",
-  messagingSenderId: "1046340307501",
-  appId: "1:1046340307501:web:ccf2f8c428c6d71efdc6a4"
+  apiKey: 'AIzaSyCEwn9-z_WS1tZgwv1fCgBe-MBJeu85fU4',
+  authDomain: 'cebin-pro.firebaseapp.com',
+  projectId: 'cebin-pro',
+  storageBucket: 'cebin-pro.firebasestorage.app',
+  messagingSenderId: '1046340307501',
+  appId: '1:1046340307501:web:ccf2f8c428c6d71efdc6a4'
 };
 
 export const app = initializeApp(firebaseConfig);
