2b37cf0775aed6dfda7f63945cc0e749b17489c4
simsektalhasefik-art <simsektalhasefik@gmail.com>
2026-08-07 11:34:17 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "BURAYA_KENDİ_API_KEY_DEĞERİNİZİ_YAZIN",
+  apiKey: "AIzaSyCEwn9-z_WS1tZgwv1fCgBe-MBJeu85fU4",
   authDomain: "cebin-pro.firebaseapp.com",
   projectId: "cebin-pro",
-  storageBucket: "cebin-pro.appspot.com",
-  messagingSenderId: "BURAYA_SENDER_ID",
-  appId: "BURAYA_APP_ID"
+  storageBucket: "cebin-pro.firebasestorage.app",
+  messagingSenderId: "1046340307501",
+  appId: "1:1046340307501:web:ccf2f8c428c6d71efdc6a4"
 };
 
 export const app = initializeApp(firebaseConfig);
